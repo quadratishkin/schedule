0fd1ef614171ee595a3533c1dd1670652c2014f9
quadratishkin <nikitavolkov.volk@mail.ru>
2025-03-21 09:48:24 +0500

    rework with parity week

--- src/data/interfaces.ts
+++ src/data/interfaces.ts
@@ -25,14 +25,14 @@ export interface ILesson {
 }
 
 export interface IScheduleDay {
-  first?: ILesson;
-  second?: ILesson;
-  third?: ILesson;
-  fourth?: ILesson;
-  fifth?: ILesson;
-  sixth?: ILesson;
-  seventh?: ILesson;
-  eighth?: ILesson;
+  first?: ILesson[];
+  second?: ILesson[];
+  third?: ILesson[];
+  fourth?: ILesson[];
+  fifth?: ILesson[];
+  sixth?: ILesson[];
+  seventh?: ILesson[];
+  eighth?: ILesson[];
 }
 
 export interface IGroup {
@@ -45,70 +45,92 @@ export interface IGroup {
 
 export const scheduleTest: IScheduleDay[] = [
   {
-    first: {
-      name: "понедельник первая пара",
-      teacher: TEACHERS[0],
-      audience: "а13",
-      week: ParityWeek.EVERYWEEK,
-      typeOfLesson: TypeOfLesson.LECTURE,
-    },
-    second: {
-      name: "понедельник вторая пара",
-      teacher: TEACHERS[1],
-      audience: 132,
-      week: ParityWeek.EVEN,
-      typeOfLesson: TypeOfLesson.PRACTICE,
-    },
+    first: [
+      {
+        name: "понедельник первая пара",
+        teacher: TEACHERS[0],
+        audience: "а13",
+        week: ParityWeek.EVERYWEEK,
+        typeOfLesson: TypeOfLesson.LECTURE,
+      },
+      {
+        name: "понедельник первая пара типа четная неделя",
+        teacher: TEACHERS[0],
+        audience: "а13",
+        week: ParityWeek.EVERYWEEK,
+        typeOfLesson: TypeOfLesson.LECTURE,
+      },
+    ],
+    second: [
+      {
+        name: "понедельник вторая пара",
+        teacher: TEACHERS[1],
+        audience: 132,
+        week: ParityWeek.EVEN,
+        typeOfLesson: TypeOfLesson.PRACTICE,
+      },
+    ],
   },
   {},
   {
-    first: {
-      name: "среда первая пара",
-      teacher: TEACHERS[0],
-      audience: "а13",
-      week: ParityWeek.EVERYWEEK,
-      typeOfLesson: TypeOfLesson.LECTURE,
-    },
-    second: {
-      name: "среда вторая пара",
-      teacher: TEACHERS[1],
-      audience: 132,
-      week: ParityWeek.EVEN,
-      typeOfLesson: TypeOfLesson.PRACTICE,
-    },
+    first: [
+      {
+        name: "среда первая пара",
+        teacher: TEACHERS[0],
+        audience: "а13",
+        typeOfLesson: TypeOfLesson.LECTURE,
+      },
+    ],
+    second: [
+      {
+        name: "среда вторая пара",
+        teacher: TEACHERS[1],
+        audience: 132,
+        week: ParityWeek.EVEN,
+        typeOfLesson: TypeOfLesson.PRACTICE,
+      },
+    ],
   },
   {
-    fifth: {
-      name: "четверг 5 пара",
-      teacher: TEACHERS[0],
-      audience: "а13",
-      week: ParityWeek.EVERYWEEK,
-      typeOfLesson: TypeOfLesson.LECTURE,
-    },
+    fifth: [
+      {
+        name: "четверг 5 пара",
+        teacher: TEACHERS[0],
+        audience: "а13",
+        week: ParityWeek.EVERYWEEK,
+        typeOfLesson: TypeOfLesson.LECTURE,
+      },
+    ],
   },
   {
-    fourth: {
-      name: "пятница 4 пара",
-      teacher: TEACHERS[0],
-      audience: "а13",
-      week: ParityWeek.EVERYWEEK,
-      typeOfLesson: TypeOfLesson.LECTURE,
-    },
-    sixth: {
-      name: "пятница 6 пара",
-      teacher: TEACHERS[0],
-      audience: "а13",
-      week: ParityWeek.EVERYWEEK,
-      typeOfLesson: TypeOfLesson.LECTURE,
-    },
+    fourth: [
+      {
+        name: "пятница 4 пара",
+        teacher: TEACHERS[0],
+        audience: "а13",
+        week: ParityWeek.EVERYWEEK,
+        typeOfLesson: TypeOfLesson.LECTURE,
+      },
+    ],
+    sixth: [
+      {
+        name: "пятница 6 пара",
+        teacher: TEACHERS[0],
+        audience: "а13",
+        week: ParityWeek.EVERYWEEK,
+        typeOfLesson: TypeOfLesson.LECTURE,
+      },
+    ],
   },
   {
-    sixth: {
-      name: "суббота 6 пара",
-      teacher: TEACHERS[0],
-      audience: "а13",
-      week: ParityWeek.EVERYWEEK,
-      typeOfLesson: TypeOfLesson.LECTURE,
-    },
+    sixth: [
+      {
+        name: "суббота 6 пара",
+        teacher: TEACHERS[0],
+        audience: "а13",
+        week: ParityWeek.EVERYWEEK,
+        typeOfLesson: TypeOfLesson.LECTURE,
+      },
+    ],
   },
 ];
